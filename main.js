b68de966a5dd275b5fd4bb32cb1ef1e9758526b2
mohammedalsomali <d.j.hamoody@gmail.com>
2020-12-02 06:57:15 -0600

    finshed resizing the canvas and fixed the color issue

--- main.js
+++ main.js
@@ -1,31 +1,25 @@
 var canvas;
 var ctx;
 var canvasWidth;
-var canvasHeight; 
+var canvasHeight;
 var lines = new Array();
-var myd = document.getElementById("mydiv");
-var adiv = document.createDocumentFragment();
+
+
 window.onload = function () {
-  //InitTests();
-  //getRandomInt();
-  // document.getElementById("btn").addEventListener("click", InitTests);
-  document.getElementById("btn").addEventListener("click", getRandomInt);
-  // document.getElementById("btn").addEventListener("click", InitTests);
-  // document.getElementById("btn").addEventListener("click", getRandomInt);
-  document.getElementById("btn2").addEventListener("click", sort);
-  // getRandomInt();
-}
-
-
-
-
-function InitTests() {
   canvas = document.getElementById('mycanvas');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
   ctx = canvas.getContext('2d');
   canvasWidth = canvas.width;
   canvasHeight = canvas.height;
-  id = ctx.getImageData(0, 0, 2, 2);
+  document.getElementById("btn").addEventListener("click", getRandomInt);
+  document.getElementById("btn2").addEventListener("click", sort);
+  
 }
+
+
+
+
 
 function clearall() {
   ctx.fillRect(0, 0, canvasWidth, canvasHeight);
@@ -33,33 +27,33 @@ function clearall() {
 }
 
 function getRandomInt() {
-  InitTests();
   clearall();
   for (var i = 0; i < canvasWidth / 10; ++i) {
     lines[i] = (Math.random() * canvasHeight);
-    
+
 
   }
   //console.log(lines)
   makedata();
-  return 
+  
 }
- function draw(a,b, i, j){
+function draw(a, b, i, j) {
   ctx.fillRect(i * 10, 0, 10, canvasHeight);
   ctx.fillRect((i + j) * 10, 0, 10, canvasHeight);
   ctx.beginPath();
-  ctx.moveTo(i * 10 , canvasHeight);
+  ctx.moveTo(i * 10, canvasHeight);
   ctx.lineWidth = 10;
-  ctx.lineTo(i * 10 , a);
+  ctx.lineTo(i * 10, a);
   ctx.strokeStyle = "blue";
   ctx.stroke();
   ctx.beginPath();
-  ctx.moveTo((j + i) * 10 , canvasHeight);
+  ctx.moveTo((j + i) * 10, canvasHeight);
   ctx.lineWidth = 10;
-  ctx.lineTo((j+i) * 10 , b);
+  ctx.lineTo((j + i) * 10, b);
   ctx.strokeStyle = "blue";
   ctx.stroke();
-  
+  i = i + 1;
+
 
 }
 
@@ -67,14 +61,18 @@ function getRandomInt() {
 
 function makedata() {
   ctx = canvas.getContext("2d");
+  ctx.fillRect(0,0, canvasWidth, canvasHeight);
   for (var i = 0; i < canvasWidth / 10; ++i) {
     ctx.beginPath();
-    ctx.moveTo(i * 10 , canvasHeight);
+    ctx.moveTo(i * 10, canvasHeight);
     ctx.lineWidth = 10;
-    ctx.lineTo(i * 10 , lines[i]);
+    ctx.lineTo(i * 10, lines[i]);
     ctx.strokeStyle = "pink";
     ctx.stroke();
+    i = i + 1;
     //console.log(h[i])
+    
+
   }
 
 }
@@ -88,20 +86,6 @@ function changecolor() {
 
 
 
-// function devide( arr, start, end){
-//   var pv = arr[end];
-//   var pi = start;
-//   for (var i; i < pv; ++i){
-//     if (arr[i] > pv){
-//       ++pi
-//       swap(arr[i], arr[end])
-//     }
-//     swap(arr[i])
-
-//   }
-
-// }
-
 function sort() {
   //ctx.fillRect(0, 0, canvasWidth, canvasHeight);
   for (var i = 0; i <= lines.length - 1; ++i) {
@@ -111,13 +95,14 @@ function sort() {
         lines[i] = lines[j + i];
         lines[i + j] = temp;
         //submit(lines[i], lines[i + j], i, j);
-        setInterval(draw, 10, lines[i],lines[i+j],i,j);
+        //setInterval(draw, 10, lines[i], lines[i + j], i, j);
+
         
-       }
+
       }
     }
-    //console.log(h)
-    //makedata();
   }
+  //console.log(h)
+  makedata();
+}
 
- 
