7da2e0f1697b67cf7d52eeabafdec2b347d8fad3
mohammedalsomali <d.j.hamoody@gmail.com>
2020-12-20 21:21:36 -0600

    added btn and centered the btns

--- main.js
+++ main.js
@@ -3,17 +3,19 @@ var ctx;
 var canvasWidth;
 var canvasHeight;
 var lines = new Array();
-
+var data = [3,4,58,9,5,2,6,5,4];
 
 window.onload = function () {
   canvas = document.getElementById('mycanvas');
   canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight * 0.95;
+  canvas.height = window.innerHeight * 0.9;
   ctx = canvas.getContext('2d');
   canvasWidth = canvas.width;
   canvasHeight = canvas.height;
   document.getElementById("btn").addEventListener("click", getRandomInt);
   document.getElementById("btn2").addEventListener("click", sort);
+  document.getElementById("btn2").addEventListener("click", Quicksort(lines));
+
   
 }
 
@@ -35,7 +37,7 @@ function getRandomInt() {
 
 
   }
-  //console.log(lines)
+  
   makedata();
   
 }
@@ -54,7 +56,6 @@ function makedata() {
     ctx.strokeStyle = "purple";
     ctx.stroke();
     i = i + 1;
-    //console.log(h[i])
     
 
   }
@@ -75,55 +76,23 @@ function changecolor() {
 async function sort() {
   
   for (var i = 0; i < lines.length ; ++i) {
-    // ctx.beginPath();
-    // ctx.moveTo(i * 10, canvasHeight);
-    // ctx.lineWidth = 10;
-    // ctx.lineTo(i * 10, lines[i]);
-    // ctx.strokeStyle = "red";
-    // ctx.stroke();
-    // await timer(10 );
     await draw(lines[i], i);
     
     for (var j = 0; j < lines.length - i; ++j) {
-      // ctx.fillstyle = "red";
-      // ctx.fillRect((i +j) * 10, 0, 10, canvasHeight);
-      // await timer(30 * j);
-      //await timer(i * 10); 
       await draw(lines[i+j], i+ j);
       
       if (lines[i] > lines[i + j]) {
-        // ctx.fillRect((i +j-1) * 10, 0, 20, canvasHeight);
         temp = lines[i];
         lines[i] = lines[j + i];
         lines[i + j] = temp;
-        //await timer( 10);
-        await drawnew(lines[i] ,lines[i + j], i, i + j );
+        await swap(lines[i] ,lines[i + j], i, i + j );
         await drawback(lines[i+j], i+j);
-      //   // ctx.fillstyle = "black";
-      //   // ctx.fillRect(i * 10, 0, 11, canvasHeight);
-      //   // await timer(30);
-      //   // ctx.fillstyle = "black";
-      //   // ctx.fillRect((j + i) * 10, 0, 11, canvasHeight);
-      //   // await timer(30 * j);
-        
-      //   // ctx.fillstyle = "blue";
-      //   // ctx.fillRect(i * 10, 0, 10, canvasHeight);
-      //   // ctx.fillstyle = "blue";
-      //   // ctx.fillRect((j + i) * 10, 0, 10, canvasHeight);
-      //   // await timer(30 * j);
+      
       }
       else{
          
         await drawback(lines[i + j], i + j);
         
-      //   // ctx.fillstyle = "pink";
-      //   // ctx.fillRect(i * 10, 0, 11, lines[i]);
-      //   // await timer(5 * j); 
-      //   // ctx.fillstyle = "pink";
-      //   // ctx.fillRect((j + i) * 10, 0, 11, lines[i+j]);
-      //   // await timer(5 * j); 
-
-
       }
         
       j = j + 1;
@@ -149,7 +118,6 @@ async function draw(b,i){
 }
 
 async function drawback(b,i){
-  //ctx.clearRect(1 - i * 10, 0, 10, canvasHeight);
   ctx.beginPath();
   ctx.moveTo(i  * 10, canvasHeight);
   ctx.lineWidth = 10;
@@ -163,7 +131,7 @@ async function drawback(b,i){
 
 
 
-async function drawnew(a,b, i,j) {
+async function swap(a,b, i,j) {
   ctx.clearRect(i * 10, 0, 10, canvasHeight);
   ctx.beginPath();
   ctx.moveTo(i * 10, canvasHeight);
@@ -171,7 +139,6 @@ async function drawnew(a,b, i,j) {
   ctx.lineTo(i * 10, a);
   ctx.strokeStyle = "blue";
   ctx.stroke();
-  await timer(10 );
   ctx.clearRect( (j-1) * 10, 0, 20, canvasHeight);
   ctx.beginPath();
   ctx.moveTo(j * 10, canvasHeight);
@@ -180,8 +147,27 @@ async function drawnew(a,b, i,j) {
   ctx.strokeStyle = "blue";
   ctx.stroke();
   await timer(100);
-  // await drawback(b, j);
   
 }
+
+
+function Quicksort(data) {
+  pivot_piont = data.lenght;
+  pivot_vlaue = data[pivot_piont];
+  for (var i = 0; i <= pivot_piont; i++) {
+    if ((data[i] >= pivot_vlaue) && (pivot_piont >= i)) {
+      temp = data[i];
+      data[i] = pivot_vlaue;
+      pivot_vlaue = temp;
+      pivot_piont = i;
+      console.log(data);
+
+    }
+  }
+
+
+}
+
+
 
 function timer(ms){return new Promise(res => setTimeout(res, ms));}
